8c86f1d498b7a0a41169caabad06fa42288dd721
ikuisma <ilkka.a.kuisma@gmail.com>
2017-12-28 15:23:32 +0200

    Added scripts for creating and dropping tables and creating test data.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -1,1 +1,8 @@
 -- Lisää INSERT INTO lauseet tähän tiedostoon
+INSERT INTO Musician (username, password) VALUES ('qsma', 'qsmapassword');
+INSERT INTO Musician (username, password) VALUES ('pekka', 'pekkapassword');
+INSERT INTO Tag (name) VALUES ('electronic');
+INSERT INTO Tag (name) VALUES ('instrumental');
+INSERT INTO Track (musician_id, title, url, description) VALUES (1, 'Clementine', 'https://soundcloud.com/qsma/clementine', 'Short track that I wrote for a cat called Clementine. ');
+INSERT INTO TrackTag (track_id, tag_id) VALUES (1, 1);
+INSERT INTO Feedback (musician_id, track_id, summary, description) VALUES (2, 1, 'This track sucks. ', 'My ears are bleeding. This sounds horrendous!');
--- sql/create_tables.sql
+++ sql/create_tables.sql
@@ -1,1 +1,32 @@
--- Lisää CREATE TABLE lauseet tähän tiedostoon
+CREATE TABLE Musician (
+  id SERIAL PRIMARY KEY,
+  username varchar(200) NOT NULL,
+  password varchar(255) NOT NULL
+);
+
+CREATE TABLE Tag(
+  id SERIAL PRIMARY KEY,
+  name varchar(50) NOT NULL UNIQUE
+);
+
+CREATE TABLE Track(
+  id SERIAL PRIMARY KEY,
+  musician_id INTEGER REFERENCES Musician(id),
+  title varchar(100) NOT NULL,
+  url varchar(100) NOT NULL,
+  description varchar(200) NOT NULL
+);
+
+CREATE TABLE Feedback(
+  id SERIAL PRIMARY KEY,
+  musician_id INTEGER REFERENCES Musician(id),
+  track_id INTEGER REFERENCES Track(id),
+  summary varchar(150),
+  description varchar(1000)
+);
+
+CREATE TABLE TrackTag(
+  track_id INTEGER REFERENCES Track(id),
+  tag_id INTEGER REFERENCES Tag(id),
+  PRIMARY KEY (track_id, tag_id)
+);
