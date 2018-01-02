5b5103be5fffc9b9d279bc058e7103afa57632f4
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-02 11:49:43 +0200

    Model class for Track

---
+++ app/models/track.php
@@ -0,0 +1,42 @@
+<?php
+
+class Track {
+
+    public $id, $title, $url, $description;
+
+    public function __construct($attributes){
+        parent::__construct($attributes);
+    }
+
+    public static function all(){
+        $query = DB::connection()->prepare('SELECT * FROM track');
+        $query->execute();
+        $rows = $query->fetchAll();
+        $tracks = array();
+        foreach($rows as $row){
+            $tracks[] = new Track(array(
+                'id' => $row['id'],
+                'title' => $row['title'],
+                'url' => $row['url'],
+                'description' => $row['description']
+            ));
+        }
+        return $tracks;
+    }
+
+    public static function find($id){
+        $query = DB::connection()->prepare('SELECT * FROM track WHERE id = :id LIMIT 1');
+        $query->execute(array('id' => $id));
+        $row = $query->fetch();
+        if($row){
+            return new Track(array(
+                'id' => $row['id'],
+                'title' => $row['title'],
+                'url' => $row['url'],
+                'description' => $row['description']
+            ));
+        }
+        return null;
+    }
+
+}
