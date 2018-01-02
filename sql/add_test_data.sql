6a0acccd29723d49c958aa784c211a899d64f13a
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-02 13:34:49 +0200

    Added more test data.

--- sql/add_test_data.sql
+++ sql/add_test_data.sql
@@ -5,4 +5,5 @@ INSERT INTO Tag (name) VALUES ('electronic');
 INSERT INTO Tag (name) VALUES ('instrumental');
 INSERT INTO Track (musician_id, title, url, description) VALUES (1, 'Clementine', 'https://soundcloud.com/qsma/clementine', 'Short track that I wrote for a cat called Clementine. ');
 INSERT INTO TrackTag (track_id, tag_id) VALUES (1, 1);
+INSERT INTO TrackTag (track_id, tag_id) VALUES (1, 2);
 INSERT INTO Feedback (musician_id, track_id, summary, description) VALUES (2, 1, 'This track sucks. ', 'My ears are bleeding. This sounds horrendous!');
