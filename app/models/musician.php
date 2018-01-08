1d1645c97f34d83dd5fb263949c8b85d4dae46c4
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-08 22:31:48 +0200

    Added saving and validation for musician model.

--- app/models/musician.php
+++ app/models/musician.php
@@ -6,6 +6,7 @@ class Musician extends BaseModel{
 
     public function __construct($attributes){
         parent::__construct($attributes);
+        $this->validators = array('validateUsername');
     }
 
     public static function all(){
@@ -48,6 +49,54 @@ class Musician extends BaseModel{
             ));
         }
         return null;
+    }
+
+    public static function findByName($username) {
+        $query = DB::connection('SELECT * FROM tag WHERE username = :username LIMIT 1');
+        $query->execute(array('name' => $username));
+        $row = $query->fetch();
+        if ($row) {
+            return new Musician(array(
+                'id' => $row['id'],
+                'username' => $row['username'],
+                'password' => $row['password']
+            ));
+        }
+        return null;
+    }
+
+    public static function musicianWithNameExists($name) {
+        return (self::findByName($name) != null);
+    }
+
+    public function save(){
+        $query = DB::connection()->prepare('INSERT INTO musician (username, password) VALUES (:username, :password) RETURNING id');
+        $password = password_hash($this->password);
+        $query->execute(array('username' => $this->username, 'password' => $password));
+        $row = $query->fetch();
+        $this->id = $row['id'];
+    }
+
+    public function validateUsername() {
+        $errors = array();
+        if ($this::emptyString($this->name)){
+            $errors[] = 'Username can not be an empty string. ';
+        }
+        if ($this::exceedsLength($this->name, 200)){
+            $errors[] = 'Username must be less than 200 characters in length. ';
+        }
+        if ($this::musicianWithNameExists){
+            $errors[] = 'Username already taken. ';
+        }
+        return $errors;
+    }
+
+    public function validatePassword() {
+        $errors = array();
+        if ($this::emptyString($this->password)) {
+            $errors[] = 'Password can not be an empty string.';
+        }
+        return $errors;
     }
 
 }
