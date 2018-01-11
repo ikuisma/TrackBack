688844abd6e25951c8967105b8f9acabb2fa1009
ikuisma <ilkka.a.kuisma@gmail.com>
2018-01-11 23:22:18 +0200

    Removed comments.

--- lib/base_controller.php
+++ lib/base_controller.php
@@ -12,9 +12,8 @@
     }
 
     public static function check_logged_in(){
-        // Toteuta kirjautumisen tarkistus tähän.
-        // Jos käyttäjä ei ole kirjautunut sisään, ohjaa hänet toiselle sivulle (esim. kirjautumissivulle).
         if (!isset($_SESSION['user'])){
+            Redirect::to('/', array('message' => 'Please log into the application!'));
         }
     }
 
